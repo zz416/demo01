cddb5f074a90655f65773423c8c1c7d49ec550d7
zz416 <1962348313@qq.com>
2020-08-19 17:02:03 +0800

    第一次向gitup推送代码

---
+++ demo01.py
@@ -0,0 +1,41 @@
+
+"""
+print("hello world") #字符串
+print(233) #整数
+print(2.333) #小数
+print(True,False) #布尔
+print(()) #元组
+print([]) #数组
+print({}) #字典
+print("哈哈哈哈"+"嘻嘻嘻嘻嘻") #字符串的拼接
+print("哈哈"*10)
+print((1+200)*2/4)
+print(2>3)
+
+a = "张三" # 把张三的值赋值给a这个变量。
+print(a)
+b = input("请输入：")
+print("input输入的值：",b)
+c = type(2333)
+print(c)
+
+
+print(type("哈哈哈哈哈"))
+print(type(22222))
+print(type(2.222))
+print(type(True))
+print(type(()))
+print(type([]))
+print(type({}))
+
+a = str(2333)
+print(type(a))
+"""
+
+
+
+a = input("请输入a:")
+a1 = len(a)
+b = input("请输入b:")
+b1 = len(b)
+print("a、b的字符串长度之和为：",len(a)+len(b))
